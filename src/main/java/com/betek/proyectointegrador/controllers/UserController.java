d04101dadd60ea05f2c8353a1367a109147d5448
aemonsal <amonsalvev@unal.edu.co>
2024-10-28 17:19:41 -0500

    fix UserController

--- src/main/java/com/betek/proyectointegrador/controllers/UserController.java
+++ src/main/java/com/betek/proyectointegrador/controllers/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
             @ApiResponse(responseCode = "200" , description = "El usuario se creo de forma exitosa",
                     content = {
                             @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = TransferResponseDto.class))
+                                    schema = @Schema(implementation = UserDto.class))
                     }),
             @ApiResponse(responseCode = "400" , description = "Hubo error de logica en la aplicacion o ingresaste mal los datos"),
     })
