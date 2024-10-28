8a3544482d446d07fa172fa7d917a83d2ddd8fb5
aemonsal <amonsalvev@unal.edu.co>
2024-10-28 16:36:58 -0500

    update readme and add pre deploy settings

--- src/main/java/com/betek/proyectointegrador/ProyectointegradorApplication.java
+++ src/main/java/com/betek/proyectointegrador/ProyectointegradorApplication.java
@@ -2,6 +2,10 @@ package com.betek.proyectointegrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectointegradorApplication {
@@ -10,4 +14,18 @@ public class ProyectointegradorApplication {
 		SpringApplication.run(ProyectointegradorApplication.class, args);
 	}
 
+	@Configuration
+	public static class CorsConfiguration{
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "POST");
+				}
+			};
+		}
+	}
 }
